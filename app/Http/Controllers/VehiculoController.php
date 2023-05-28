87bf8ac6d3f03650e41e8091b213a6301a69e5cd
nixonBravo <nixonelian2002@gmail.com>
2023-05-28 18:14:49 -0500

    Migracion Realizada

---
+++ app/Http/Controllers/VehiculoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vehiculo;
+use Illuminate\Http\Request;
+
+class VehiculoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Vehiculo $vehiculo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Vehiculo $vehiculo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Vehiculo $vehiculo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Vehiculo $vehiculo)
+    {
+        //
+    }
+}
